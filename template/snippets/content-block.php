634c177efe326e2e9b7198d93bb91e365fad2d37
Tarzanych <tarzanych@gmail.com>
2013-12-18 12:16:58 +0200

    Зміни, які стосуються локалізації і форматування

--- template/snippets/content-block.php
+++ template/snippets/content-block.php
@@ -1,12 +1,18 @@
 <div class="contentBlock">
     <div class="contentTitle">
         <a href="<?= $fullUrl ?>"><?= $cont['contentTitle'] ?></a>
-        <? if ($User->CheckPermissions('edit') || ($User->CheckPermissions('editOwn') && $cont['author'] == $User->Id)) { ?><a href="#" onclick="editContent(<?= $cont['id'] ?>);
-                    return false;"><img src="<?= $this->RootUrl ?>template/img/edit.png" alt="Edit" title="Edit" /></a><? } ?>
-        <? if ($User->CheckPermissions('delete') || ($User->CheckPermissions('deleteOwn') && $cont['author'] == $User->Id)) { ?><a href="#" onclick="if (confirm('Are you sure?')) {
+        <? if ($User->CheckPermissions('edit') || ($User->CheckPermissions('editOwn') && $cont['author'] == $User->Id)) { ?>
+            <a href="#" onclick="editContent(<?= $cont['id'] ?>);
+                    return false;">
+                <img src="<?= $this->RootUrl ?>template/img/edit.png" alt="Edit" title="Edit" />
+            </a><? } ?>
+        <? if ($User->CheckPermissions('delete') || ($User->CheckPermissions('deleteOwn') && $cont['author'] == $User->Id)) { ?>
+            <a href="#" onclick="if (confirm('Are you sure?')) {
                         deleteContent(<?= $cont['id'] ?>);
                     }
-                    return false;"><img src="<?= $this->RootUrl ?>template/img/delete.png" alt="Edit" title="Edit" /></a><? } ?>
+                    return false;">
+                <img src="<?= $this->RootUrl ?>template/img/delete.png" alt="Edit" title="Edit" />
+            </a><? } ?>
     </div>
     <div class="contentText">
         <?= ($cont['contentPreText'] ? $cont['contentPreText'] : (mb_strlen($cont['contentTotalText']) > 150 ? substr($cont['contentTotalText'], 0, 150) . "..." : $cont['contentTotalText'])) ?>
